d7b8302866d7b7d51c6091064cf13f4dc9a86156
Randy Nava <randyn1081@gmail.com>
2025-04-30 16:08:55 +0000

    empty commit test

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,7 +1,7 @@
 package com.example.controller;
 
 
-/**
+/** test
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
  * found in readme.md as well as the test cases. You be required to use the @GET/POST/PUT/DELETE/etc Mapping annotations
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
