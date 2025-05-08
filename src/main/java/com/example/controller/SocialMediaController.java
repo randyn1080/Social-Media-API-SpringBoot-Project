7d8093b2b2bb335f0c516fdf68897d1420ca6806
Randy Nava <randyn1081@gmail.com>
2025-05-08 20:26:51 +0000

    implemented POST message api path

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -7,15 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.service.*;
 import com.example.entity.*;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-/** test
- * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
- * found in readme.md as well as the test cases. You be required to use the @GET/POST/PUT/DELETE/etc Mapping annotations
- * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
- * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
- */
 @RestController
 public class SocialMediaController {
     private final AccountService accountService;
@@ -37,5 +30,11 @@ public class SocialMediaController {
     public ResponseEntity<Account> login(@RequestBody Account account) {
         Account loggedInAccount = accountService.login(account);
         return ResponseEntity.ok(loggedInAccount);
+    }
+
+    @PostMapping("messages")
+    public ResponseEntity<Message> createMessage(@RequestBody Message message) {
+        Message newMessage = messageService.createMessage(message);
+        return ResponseEntity.ok(newMessage);
     }
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,7 +1,44 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.entity.Message;
+import com.example.exception.InvalidMessageException;
+import com.example.repository.AccountRepository;
+import com.example.repository.MessageRepository;
 
 @Service
 public class MessageService {
+    private final MessageRepository messageRepository;
+    private final AccountRepository accountRepository;
+
+    @Autowired
+    public MessageService(MessageRepository messageRepository, AccountRepository accountRepository) {
+        this.messageRepository = messageRepository;
+        this.accountRepository = accountRepository;
+    }
+
+    public Message createMessage(Message message) {
+        String messageText = message.getMessageText();
+        // check if the text is valid
+        if (messageText == null || messageText.isBlank()) {
+            throw new InvalidMessageException("Message text cannot be blank");
+        }
+
+        // check for length
+        if (messageText.length() > 255) {
+            throw new InvalidMessageException("Message text must be less than 255 characters");
+        }
+
+        // check if the account exists
+        boolean accountExists = accountRepository.existsById(message.getPostedBy());
+
+        if (!accountExists) {
+            throw new InvalidMessageException("Account does not exist");
+        }
+
+        // save the message and return it with the generated ID
+        return messageRepository.save(message);
+    }
 }
