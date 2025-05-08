3997aa5e84618ef16479c75cd4d7dba6948bbfd7
Randy Nava <randyn1081@gmail.com>
2025-05-08 22:38:25 +0000

    implemented GET accounts/{accountId}/messages api path

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -3,7 +3,9 @@ package com.example.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.example.entity.Message;
+import java.util.List;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Integer>{
+    List<Message> findMessagesByPostedBy(Integer accountId);
 }
